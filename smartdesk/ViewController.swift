5a1355b6bd944427f03746ead7a76c28e23892b8
Jing Wei Li <ljw9801055@gmail.com>
2018-10-10 00:45:05 -0400

    Initial Commit

---
+++ smartdesk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smartdesk
+//
+//  Created by Jing Wei Li on 10/10/18.
+//  Copyright © 2018 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
